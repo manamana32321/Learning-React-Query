d77f68ae9683dba5c2b074559a00af6c4d313716
manamana32321 <manamana32321@gmail.com>
2023-08-04 14:12:47 +0900

    Implement 'useMutation'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation } from '@tanstack/react-query'
 
-const POSTS = [
-  { id: 1, title: 'Post 1' },
-  { id: 2, title: 'Post 2' },
+interface Post {
+  id: string
+  title: string
+}
+
+const POSTS : Post[] = [
+  { id: "1", title: 'Post 1' },
+  { id: "2", title: 'Post 2' },
 ]
+
 function App() {
   const postsQuery = useQuery({
     queryKey: ["posts"],
     // queryFn: () => Promise.reject("Error Message"),
     queryFn: () => wait(1000).then(() => POSTS),
+  })
+
+  const newPostMutation = useMutation({
+    mutationFn: (title: string) => {
+      return wait(1000).then(() => {
+        POSTS.push({ id: crypto.randomUUID(), title })
+      })
+    }
   })
 
   if (postsQuery.isLoading) return <h1>Loading...</h1>
@@ -22,6 +36,9 @@ function App() {
         {post.title}
       </div>
     ))}
+    <button onClick={() => newPostMutation.mutate("New Post")}>
+      Add Post
+    </button>
   </div>
 }
 
