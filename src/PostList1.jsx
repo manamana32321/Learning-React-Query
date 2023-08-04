54b31e93ec70f9382532242e66c824385a9d44b6
manamana32321 <manamana32321@gmail.com>
2023-08-04 21:31:29 +0900

    Initial/Placeholder Data

--- src/PostList1.jsx
+++ src/PostList1.jsx
@@ -6,19 +6,21 @@ export default function PostList1() {
   const postsQuery = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
+    placeholderData: [{ id: 1, title: "Placeholder Post"}],
+    initialData: [{ id: 1, title: "Initial Post"}],
     // staleTime: 1000 * 60 * 5,
     // refetchInterval: 1000,
   })
 
-  const queries = useQueries({
-    queries: (postsQuery?.data ?? []).map(post =>  {
-    return {
-      queryKey: ["post", post.id],
-      queryFn: () => getPost(post.id),
-    }
-  })})
+  // const queries = useQueries({
+  //   queries: (postsQuery?.data ?? []).map(post =>  {
+  //   return {
+  //     queryKey: ["post", post.id],
+  //     queryFn: () => getPost(post.id),
+  //   }
+  // })})
 
-  console.log(queries.map(query => query.data))
+  // console.log(queries.map(query => query.data))
 
   if (postsQuery.isLoading) return <h1>Loading...</h1>
   if (postsQuery.isError) {
